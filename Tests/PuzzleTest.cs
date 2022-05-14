518884e4854d05acb4f812a4556b753a1b577c13
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2022-05-14 15:06:34 +0300

    add first tests for puzzle service

---
+++ Tests/PuzzleTest.cs
@@ -0,0 +1,91 @@
+using Xunit;
+using System.Linq;
+using System;
+
+namespace Tests;
+
+public class UnitTest1
+{
+    private readonly PuzzleVM puzzle = new PuzzleVM("Hard", "my puzzle", "a description");
+
+    private readonly PuzzleService ps = new PuzzleService();
+
+    [Fact]
+    public void ShouldAddPuzzleAndDeleteIt()
+    {
+        // act
+        Puzzle created = ps.AddPuzzle(puzzle);
+
+        // assert
+        Assert.True(created.id != Guid.Empty);
+        
+        // clean up
+        ps.DeletePuzzle(created.id);
+    }
+
+    [Fact]
+    public void ShouldGetAllPuzzles()
+    {
+        // arrange
+        var created = ps.AddPuzzle(puzzle);
+
+        // act
+        var puzzles = ps.GetPuzzleList();
+
+        // assert
+        Assert.True(puzzles.Count() > 0);
+
+        //clean up
+        ps.DeletePuzzle(created.id);
+    }
+
+    [Theory]
+    [InlineData("ddf1cb54-53d2-45bb-b01a-f2eeae7cb249")] // random one
+    [InlineData("00000000-0000-0000-0000-000000000000")] // empty one
+    public void ShouldNotDeletePuzzle(string id)
+    {
+        // arrange
+        var guid = Guid.Parse(id);
+        
+        // act
+        var deleted = ps.DeletePuzzle(guid);
+
+        // assert
+        Assert.False(deleted);
+    }
+
+    [Fact]
+    public void ShouldAddUpdateAndDeleteAPuzzle()
+    {
+        // arrange
+        Puzzle created = ps.AddPuzzle(puzzle);
+        puzzle.id = created.id;
+        puzzle.name = "Updated puzzle";
+
+        // act
+        bool updated = ps.UpdatePuzzle(puzzle);
+
+        // assert
+        Assert.True(updated);
+
+        // clean up
+        ps.DeletePuzzle(puzzle.id);
+    }
+
+    [Theory]
+    [InlineData("ddf1cb54-53d2-45bb-b01a-f2eeae7cb249")] // random one
+    [InlineData("00000000-0000-0000-0000-000000000000")] // empty one
+    public void ShouldNotUpdatePuzzle(string id)
+    {
+        // arrange
+        var guid = Guid.Parse(id);
+        puzzle.id = guid;
+        puzzle.name = "updated puzzle";
+
+        // act
+        var updated = ps.UpdatePuzzle(puzzle);
+        
+        // assert
+        Assert.False(updated);
+    }
+}
